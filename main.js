fef697b8e7e1ddee46ab7986a9a8e450865330ef
evanZccc <167677714+evanZccc@users.noreply.github.com>
2025-03-24 00:01:11 +0100

    main.js

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ const { YtDlpPlugin } = require('@distube/yt-dlp');
 const { SpotifyPlugin } = require('@distube/spotify');
 const { SoundCloudPlugin } = require('@distube/soundcloud');
 const config = require('./config.json');
-const musicIcons = require('./UI/icons/musicicons');
 const colors = require('./UI/colors/colors');
 const loadLogHandlers = require('./logHandlers');
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
